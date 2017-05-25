674a1e440ae5cbe2312848bf01e3e5586c80d569
Alex Herdzik <aherdzik@daemen.edu>
2017-05-25 14:31:43 -0400

    Font

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous" type="text/javascript">
 </script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous" type="text/javascript">
@@ -40,7 +41,6 @@
       background-image: url('assets/images/turin.jpg');
       background-attachment: fixed;
       background-repeat: no-repeat;
-      background-size:cover;
       color: #fff;
       min-height: 200px;
   }
@@ -60,6 +60,7 @@
       position: absolute;
       bottom: 15px;
       left: 15px;
+      font-family: 'Roboto', sans-serif;
   }
   
   /* Toggle Styles */
